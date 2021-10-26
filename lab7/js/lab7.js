d6178c8f17b671bbd28c662cd839ed590ce4dc13
G4m3rpri3st <61484305+G4m3rpri3st@users.noreply.github.com>
2021-10-26 00:40:01 -0700

    lab 7 complete

---
+++ lab7/js/lab7.js
@@ -0,0 +1,36 @@
+/* Lab 7: Functions.
+* Created: 10.25.2021
+* @author Logan Flansaas and Lyza Stevens.
+* @since  10.25.2021
+* License: Public Domain.
+*/
+//define variables
+var userName = window.prompt("Hello! Please enter your name below.");
+
+// sortUserName - function that takes user input and sorts the letters of their name
+function sortUserName(name) {
+  console.log("userName =", name);
+  //split string to array
+  var nameArray = name.split(" ").join('').split('');
+  console.log("nameArray =", nameArray);
+  //sort the array
+  var nameArraySort = nameArray.sort(compareWords);
+  console.log("nameArraySort =", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//copied from compareWords function in source: https://dev.to/jenshaw/sorting-out-javascript-sort-4kbl
+function compareWords(a,b) {
+  if (a.toLowerCase() < b.toLowerCase()) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+//output
+document.writeln("Thank you, ", userName, ". Here is your sorted name: ",
+  sortUserName(userName), "</br>");
