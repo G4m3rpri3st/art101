ca07abd50ff7e5195e226d8c6eae57aaf0ce0c55
G4m3rpri3st <61484305+G4m3rpri3st@users.noreply.github.com>
2021-11-15 16:05:59 -0800

    Update lab12.js

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -5,9 +5,12 @@
 * License: Public Domain.
 */
 
+//button variable
 var buttonEl = document.getElementById("button");
+//output variable
 var outputEl = document.getElementById("output");
 
+//sorts a name into a Hogwarts House
 function sortingHat(str){
   var length = str.length;
   var mod = length % 4;
@@ -15,7 +18,10 @@ function sortingHat(str){
   return house[mod];
 }
 
+//event listener for button
 buttonEl.addEventListener("click", function(){
+  //variable for text field
   var name = document.getElementById("input");
+  //outputs the sorted name to the page
   outputEl.innerHTML = "The Sorting Hat has sorted you into " + sortingHat(name.value);
 })
