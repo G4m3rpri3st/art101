3abd41980aa3bd79269cd1ec1cd51780e3944601
G4m3rpri3st <61484305+G4m3rpri3st@users.noreply.github.com>
2021-11-30 00:52:00 -0800

    labs

---
+++ lab14/js/lab14.js
@@ -0,0 +1,6 @@
+/* Lab 14: Debugging Tools and Strategies
+* Created: 11.29.2021
+* @author Logan Flansaas and Lyza Stevens.
+* @since  11.29.2021
+* License: Public Domain.
+*/
---
+++ lab16/js/lab16.js
@@ -0,0 +1,38 @@
+/* Lab 16: JSON and APIs
+* Created: 12.2.2021
+* @author Logan Flansaas and Lyza Stevens.
+* @since  12.2.2021
+* License: Public Domain.
+*/
+
+let oneLongString = "";
+
+for (let i = 1; i < 201; i++) {
+
+  if ((i%3 == 0) && (i%5 == 0) && (i%7 == 0)){
+    oneLongString += i + " FizzBuzzBoom!<br>";
+  }
+  else if ((i%3 == 0) && (i%5 == 0)){
+    oneLongString += i + " FizzBuzz!<br>";
+  }
+  else if ((i%3 == 0) && (i%7 == 0)){
+    oneLongString += i + " FizzBoom!<br>";
+  }
+  else if ((i%5 == 0) && (i%7 == 0)){
+    oneLongString += i + " BuzzBoom!<br>";
+  }
+  else if ((i%3 == 0)){
+    oneLongString += i + " Fizz!<br>";
+  }
+  else if ((i%5 == 0)){
+    oneLongString += i + " Buzz!<br>";
+  }
+  else if ((i%7 == 0)){
+    oneLongString += i + " Boom!<br>";
+  }
+  else{
+    oneLongString += i + " <br>";
+  }
+}
+
+$("#output").html(oneLongString);
