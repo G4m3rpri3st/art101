1d285e165cb9ea7a9387d6960f71b5284dd24068
G4m3rpri3st <61484305+G4m3rpri3st@users.noreply.github.com>
2021-12-01 20:03:12 -0800

    lab16

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -5,34 +5,57 @@
 * License: Public Domain.
 */
 
-let oneLongString = "";
+var myUrl = "https://xkcd.com/info.0.json"
 
-for (let i = 1; i < 201; i++) {
+let comicObj = {
+month: "",
+num: 0,
+link: "",
+year: "",
+news: "",
+safe_title: "",
+transcript: "",
+alt: "",
+img: "",
+title: "",
+day: ""
+};
 
-  if ((i%3 == 0) && (i%5 == 0) && (i%7 == 0)){
-    oneLongString += i + " FizzBuzzBoom!<br>";
-  }
-  else if ((i%3 == 0) && (i%5 == 0)){
-    oneLongString += i + " FizzBuzz!<br>";
-  }
-  else if ((i%3 == 0) && (i%7 == 0)){
-    oneLongString += i + " FizzBoom!<br>";
-  }
-  else if ((i%5 == 0) && (i%7 == 0)){
-    oneLongString += i + " BuzzBoom!<br>";
-  }
-  else if ((i%3 == 0)){
-    oneLongString += i + " Fizz!<br>";
-  }
-  else if ((i%5 == 0)){
-    oneLongString += i + " Buzz!<br>";
-  }
-  else if ((i%7 == 0)){
-    oneLongString += i + " Boom!<br>";
-  }
-  else{
-    oneLongString += i + " <br>";
-  }
+function callAjax() {
+// Using the core $.ajax() method
+$.ajax({
+    // API endpoint
+    url: myUrl,
+    // POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json"
+})
+// If the request succeeds
+// data is passed back
+.done(function(data) {
+    console.log("Success:", data);
+    comicObj.month = data.month;
+    comicObj.num = data.num;
+    comicObj.link = data.link;
+    comicObj.year = data.year;
+    comicObj.news = data.news;
+    comicObj.safe_title = data.safe_title;
+    comicObj.transcript = data.transcript;
+    comicObj.alt = data.alt;
+    comicObj.img = data.img;
+    comicObj.title = data.title;
+    comicObj.day = data.day;
+    $("div#output h2").html(comicObj.title);
+    $("#output").append('<img id="comic" src="" title="" alt="" />');
+    $("#comic").attr("src", comicObj.img);
+    $("#comic").attr("title", comicObj.title);
+    $("#comic").attr("alt", comicObj.alt);
+})
+// If the request fails
+.fail(function(request,error) {
+    console.log(request, error);
+})
 }
 
-$("#output").html(oneLongString);
+$("#activate").click(callAjax);
